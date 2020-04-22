16c344f958bc2d1e0f8c05068d034e9f83186932
matheus.geiser <matheus.geiser@hbsis.com.br>
2020-04-22 16:38:28 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:gif_app/ui/HomePage.dart';
+
+void main() {
+
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+}
